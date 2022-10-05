5ce5f6215aac84ae1016a21e3c3b18f05837f080
arpita <103961103+1410Arpita@users.noreply.github.com>
2022-10-04 23:12:01 -0700

    Time: 0 ms, Memory: 44.9 MB - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.java
+++ linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -18,6 +18,7 @@ public class Solution {
         while (fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
+            
             if (slow == fast){
                 break;
             }
